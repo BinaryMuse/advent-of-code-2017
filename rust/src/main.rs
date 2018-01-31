9d72fb72e92f55cf01595a66c3338d9daaaec389
Michelle Tilley <binarymuse@github.com>
2018-01-31 11:18:36 -0700

    Fixup main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -14,6 +14,8 @@ mod day13;
 mod day14;
 mod day15;
 mod day16;
+mod day17;
+mod day18;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -39,6 +41,8 @@ fn main() {
         14 => day14::run,
         15 => day15::run,
         16 => day16::run,
+        17 => day17::run,
+        18 => day18::run,
         _  => {
             println!("Couldn't find implementation for day {}", day);
             std::process::exit(1);
